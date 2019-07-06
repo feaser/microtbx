daa382337055e62da0487ce4d01b922eafe73f3f
Frank Voorburg <voorburg@feaser.com>
2019-07-06 11:46:00 +0200

    Added framework for MkDocs and the generic header file.

---
+++ docs/index.md
@@ -0,0 +1,9 @@
+MicroTBX is an open source Microcontroller ToolBoX consisting of software components commonly needed in embedded software applications. Think of it as a Swiss army knife for your firmware.
+
+# Installation
+
+TODO
+
+# Usage
+
+TODO
---
+++ source/microtbx.h
@@ -0,0 +1,61 @@
+/************************************************************************************//**
+* \file         microtbx.h
+* \brief        MicroTBX glocal header file.
+* \internal
+*----------------------------------------------------------------------------------------
+*                          C O P Y R I G H T
+*----------------------------------------------------------------------------------------
+*   Copyright (c) 2019  by Feaser    http://www.feaser.com    All rights reserved
+*
+*----------------------------------------------------------------------------------------
+*                            L I C E N S E
+*----------------------------------------------------------------------------------------
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* \endinternal
+****************************************************************************************/
+#ifndef MICROTBX_H
+#define MICROTBX_H
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include <stdint.h>                         /* for standard integer types              */
+#include <stddef.h>                         /* for standard definitions                */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/****************************************************************************************
+* Macro definitions
+****************************************************************************************/
+/** \brief Boolean true value. */
+#define TBX_TRUE       (1u)
+
+/** \brief Boolean false value. */
+#define TBX_FALSE      (0u)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MICROTBX_H */
+/*********************************** end of microtbx.h *********************************/
