27f44b2ec78e27d896ed680fbcecb37c7f9ee698
Frank Voorburg <voorburg@feaser.com>
2019-07-06 12:26:52 +0200

    Added demo framework for the Nucleo-F091RC board.

---
+++ demos/ARMCM0_STM32F0_Nucleo_F091RC_GCC/led.c
@@ -0,0 +1,122 @@
+/************************************************************************************//**
+* \file         demos/ARMCM0_STM32F0_Nucleo_F091RC_GCC/led.c
+* \brief        LED driver source file.
+* \internal
+*----------------------------------------------------------------------------------------
+*                          C O P Y R I G H T
+*----------------------------------------------------------------------------------------
+*   Copyright (c) 2019  by Feaser    http://www.feaser.com    All rights reserved
+*
+*----------------------------------------------------------------------------------------
+*                            L I C E N S E
+*----------------------------------------------------------------------------------------
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* \endinternal
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include <stdint.h>                              /* for standard integer types         */
+#include <stdbool.h>                             /* for standard boolean types         */
+#include "led.h"                                 /* LED driver                         */
+#include "stm32f0xx.h"                           /* STM32 CPU and HAL header           */
+
+
+/****************************************************************************************
+* Local data declarations
+****************************************************************************************/
+/** \brief Current state of the LED. Boolean true for ON, boolean false for off. */
+static bool ledState;
+
+
+/************************************************************************************//**
+** \brief     Initializes the LED driver.
+** \return    none.
+**
+****************************************************************************************/
+void LedInit(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* Initialize locals. */
+  ledState = false;
+
+  /* GPIO ports clock enable. */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /* Configure the LED GPIO pin. */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  /* turn the LED off */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+} /*** end of LedInit ***/
+
+
+/************************************************************************************//**
+** \brief     Controls the state of the LED.
+** \param     on Boolean true to turn the LED on, boolean false for off.
+** \return    none.
+**
+****************************************************************************************/
+void LedSet(bool on)
+{
+  if (on == true)
+  {
+    /* turn the LED on */
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+    ledState = true;
+  }
+  else
+  {
+    /* turn the LED off */
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+    ledState = false;
+  }
+} /*** end of LedSet ***/
+
+
+/************************************************************************************//**
+** \brief     Obtains the current state of the LED.
+** \return    Boolean true if the LED is on, boolean false if off.
+**
+****************************************************************************************/
+bool LedGet(void)
+{
+  return ledState;
+} /*** end of LedGet ***/
+
+
+/************************************************************************************//**
+** \brief     Toggles the current state of the LED.
+** \return    none.
+**
+****************************************************************************************/
+void LedToggle(void)
+{
+  /* Toggle the LED state. */
+  LedSet(!LedGet());
+} /*** end of LedToggle ***/
+
+
+/*********************************** end of led.c **************************************/
---
+++ demos/ARMCM0_STM32F0_Nucleo_F091RC_GCC/main.c
@@ -0,0 +1,161 @@
+/************************************************************************************//**
+* \file         demos/ARMCM0_STM32F0_Nucleo_F091RC_GCC/main.c
+* \brief        Demo program application source file.
+* \internal
+*----------------------------------------------------------------------------------------
+*                          C O P Y R I G H T
+*----------------------------------------------------------------------------------------
+*   Copyright (c) 2019  by Feaser    http://www.feaser.com    All rights reserved
+*
+*----------------------------------------------------------------------------------------
+*                            L I C E N S E
+*----------------------------------------------------------------------------------------
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* \endinternal
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include <stdint.h>                              /* for standard integer types         */
+#include <stdbool.h>                             /* for standard boolean types         */
+#include "timer.h"                               /* timer driver                       */
+#include "led.h"                                 /* LED driver                         */
+#include "stm32f0xx.h"                           /* STM32 CPU and HAL header           */
+
+
+/****************************************************************************************
+* Function prototypes
+****************************************************************************************/
+static void Init(void);
+static void SystemClock_Config(void);
+
+
+/************************************************************************************//**
+** \brief     This is the entry point for the bootloader application and is called
+**            by the reset interrupt vector after the C-startup routines executed.
+** \return    Program exit code.
+**
+****************************************************************************************/
+int main(void)
+{
+  uint32_t lastLedToggleTime = 0;
+
+  /* Initialize the microcontroller. */
+  Init();
+
+  /* Start the infinite program loop. */
+  while (1)
+  {
+    /* Toggle the LED every 500 ms. */
+    if ((TimerGet() - lastLedToggleTime) >= 500u)
+    {
+      LedToggle();
+      lastLedToggleTime += 500;
+    }
+  }
+  /* Set program exit code. Note that the program should never get here. */
+  return 0;
+} /*** end of main ***/
+
+
+/************************************************************************************//**
+** \brief     Initializes the microcontroller.
+** \return    none.
+**
+****************************************************************************************/
+static void Init(void)
+{
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+  /* Configure the system clock. */
+  SystemClock_Config();
+  /* Initialize the timer driver. */
+  TimerInit();
+  /* Initialize the LED driver. */
+  LedInit();
+} /*** end of Init ***/
+
+
+/************************************************************************************//**
+** \brief     System Clock Configuration. This code was created by CubeMX and configures
+**            the system clock.
+** \return    none.
+**
+****************************************************************************************/
+static void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+  /* Initializes the CPU, AHB and APB busses clocks. */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Clock configuration incorrect or hardware failure. Hang the system to prevent
+     * damage.
+     */
+    while(1);
+  }
+
+  /* Initializes the CPU, AHB and APB busses clocks. */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  /* Set the flash latency. */
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    /* Flash latency configuration incorrect or hardware failure. Hang the system to
+     * prevent damage.
+     */
+    while(1);
+  }
+} /*** end of SystemClock_Config ***/
+
+
+/************************************************************************************//**
+** \brief     Initializes the Global MSP. This function is called from HAL_Init()
+**            function to perform system level initialization (GPIOs, clock, DMA,
+**            interrupt).
+** \return    none.
+**
+****************************************************************************************/
+void HAL_MspInit(void)
+{
+  /* SYSCFG clock enable. */
+  __HAL_RCC_SYSCFG_CLK_ENABLE();
+
+  /* SVC_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SVC_IRQn, 0, 0);
+  /* PendSV_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(PendSV_IRQn, 0, 0);
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+} /*** end of HAL_MspInit ***/
+
+
+/*********************************** end of main.c *************************************/
