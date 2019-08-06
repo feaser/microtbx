eda0fd6c467bc0edb78eb6429b2d85e8878c5694
Frank Voorburg <voorburg@feaser.com>
2019-08-06 11:58:19 +0200

    Minor correction.

--- source/port/ARM_CORTEXM/IAR/tbxcomp.s
+++ source/port/ARM_CORTEXM/IAR/tbxcomp.s
@@ -31,6 +31,7 @@
 ;* \endinternal
 ;****************************************************************************************
 
+
         PUBLIC  TbxPortInterruptsDisable
         PUBLIC  TbxPortInterruptsRestore
 
