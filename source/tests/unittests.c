e621fb2e4cf5b0730447ef29e4f2712e210a4238
Frank Voorburg <voorburg@feaser.com>
2022-06-10 18:56:58 +0200

    Reverted temporary change.

--- source/tests/unittests.c
+++ source/tests/unittests.c
@@ -210,7 +210,7 @@ void test_TbxGeneric_BooleanMacrosShouldBePresent(void)
 void test_TbxGeneric_UnusedArgMacroShouldBePresent(void)
 {
   /* Check presence of the unused argument macro. */
-  #ifndef TBX_UNUSED_ARG2
+  #ifndef TBX_UNUSED_ARG
   TEST_FAIL();
   #endif
 } /*** end of test_TbxGeneric_UnusedArgMacroShouldBePresent ***/
