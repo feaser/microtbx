f92405483811943344550df4b55f7f5c382f5313
Frank Voorburg <voorburg@feaser.com>
2023-02-03 20:07:03 +0100

    Switched to CppCheck for MISRA compliance checking.

---
+++ source/misra.cmake
@@ -0,0 +1,49 @@
+# Create target for MISRA checking MicroTBX if cppcheck can be located.
+find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
+if (CMAKE_CXX_CPPCHECK)
+    # Collect MicroTBX include directories.
+    get_target_property(microtbx_incs microtbx INTERFACE_INCLUDE_DIRECTORIES)
+    # Collect MicroTBX Cortex-M port include directories.
+    get_target_property(microtbx_cortexm_incs microtbx-cortexm INTERFACE_INCLUDE_DIRECTORIES)
+    # Collect MicroTBX template include directories.
+    get_target_property(microtbx_template_incs microtbx-template INTERFACE_INCLUDE_DIRECTORIES)
+
+    # Build list with search paths for include files and transform by adding a "-I".
+    set(search_path_incs)
+    list(APPEND search_path_incs ${microtbx_incs})
+    list(APPEND search_path_incs ${microtbx_cortexm_incs})
+    list(APPEND search_path_incs ${microtbx_template_incs})
+    list(TRANSFORM search_path_incs PREPEND "-I")
+
+    # Build list with MicroTBX target sources to check.
+    # TODO Add Cortex-M port sources.
+    get_target_property(microtbx_srcs microtbx INTERFACE_SOURCES)
+
+    # Set variable pointing to the addon for configuring the MISRA checks.
+    set(misra_addon "${CMAKE_CURRENT_LIST_DIR}/misra.json")
+
+    # Build list with cppcheck commands, one for each given source file
+    set(cppcheck_commands)
+    foreach(sourcefile ${microtbx_srcs})
+        # create command line for running cppcheck on one source file and add it
+        # to the list of commands
+        list(APPEND cppcheck_commands
+            COMMAND ${CMAKE_CXX_CPPCHECK}
+            --platform=arm32-wchar_t2.xml
+            --addon=${misra_addon}
+            --relative-paths
+            --error-exitcode=1
+            --language=c
+            --std=c11
+            --inline-suppr 
+            --suppress=missingIncludeSystem
+            --suppress=unmatchedSuppression
+            --enable=warning,style,information,missingInclude
+            -DDEBUG
+            ${search_path_incs}
+            ${sourcefile})
+    endforeach(sourcefile)
+
+    # add a custom target consisting of all the commands generated above
+    add_custom_target(microtbx_MISRA ${cppcheck_commands} VERBATIM)
+endif()
