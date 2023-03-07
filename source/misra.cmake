1e519bca60ef78685f10f58a2b1b980e767911dc
Frank Voorburg <voorburg@feaser.com>
2023-03-07 23:13:13 +0100

    Added AVR port.

--- source/misra.cmake
+++ source/misra.cmake
@@ -7,6 +7,8 @@ if (CMAKE_CXX_CPPCHECK)
     get_target_property(microtbx_cortexm_incs microtbx-cortexm INTERFACE_INCLUDE_DIRECTORIES)
     # Collect MicroTBX RP2040 port include directories.
     get_target_property(microtbx_rp2040_incs microtbx-rp2040 INTERFACE_INCLUDE_DIRECTORIES)
+    # Collect MicroTBX AVR port include directories.
+    get_target_property(microtbx_avr_incs microtbx-avr INTERFACE_INCLUDE_DIRECTORIES)
     # Collect MicroTBX template include directories.
     get_target_property(microtbx_template_incs microtbx-template INTERFACE_INCLUDE_DIRECTORIES)
 
@@ -15,6 +17,7 @@ if (CMAKE_CXX_CPPCHECK)
     list(APPEND search_path_incs ${microtbx_incs})
     list(APPEND search_path_incs ${microtbx_cortexm_incs})
     list(APPEND search_path_incs ${microtbx_rp2040_incs})
+    list(APPEND search_path_incs ${microtbx_avr_incs})
     list(APPEND search_path_incs ${microtbx_template_incs})
     list(TRANSFORM search_path_incs PREPEND "-I")
 
@@ -24,12 +27,15 @@ if (CMAKE_CXX_CPPCHECK)
     get_target_property(microtbx_cortexm_srcs microtbx-cortexm INTERFACE_SOURCES)
     # Collect MicroTBX RP2040 port sources.
     get_target_property(microtbx_rp2040_srcs microtbx-rp2040 INTERFACE_SOURCES)
+    # Collect MicroTBX AVR port sources.
+    get_target_property(microtbx_avr_srcs microtbx-avr INTERFACE_SOURCES)
 
     # Build list with MicroTBX sources to check.
     set(check_srcs)
     list(APPEND check_srcs ${microtbx_srcs})
     list(APPEND check_srcs ${microtbx_cortexm_srcs})
     list(APPEND check_srcs ${microtbx_rp2040_srcs})
+    list(APPEND check_srcs ${microtbx_avr_srcs})
 
     # Set variable pointing to the addon for configuring the MISRA checks.
     set(misra_addon "${CMAKE_CURRENT_LIST_DIR}/misra.json")
