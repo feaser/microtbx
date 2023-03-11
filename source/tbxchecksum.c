f022fc8a59b6f1484bc1cc4bf6a387ba0a8d9232
Frank Voorburg <voorburg@feaser.com>
2023-03-11 12:36:42 +0100

    Added SPDX license identifier.

--- source/tbxchecksum.c
+++ source/tbxchecksum.c
@@ -13,6 +13,9 @@
 *----------------------------------------------------------------------------------------
 *                            L I C E N S E
 *----------------------------------------------------------------------------------------
+*
+* SPDX-License-Identifier: MIT
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
